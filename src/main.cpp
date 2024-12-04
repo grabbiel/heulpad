5b75beab4815c3b154f8bedccfa9bb94e136c51e
grabbiel <fcruzado22@gmail.com>
2024-12-04 15:45:49 -0800

    testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
+#include <cstdio>
+#include <cstdlib>
 
 int main(int argc, char *argv[]) {
-  char *str;
+  const char *config_path = std::getenv("HEULPAD_CONFIG");
+  if (!config_path) {
+    printf("Could not find config path\n");
+  } else {
+    printf("%s\n", config_path);
+  }
   return 0;
 }
