dcabf0864b61ce9a400a71657fa31a532708a93f
grabbiel <fcruzado22@gmail.com>
2024-12-04 19:38:53 -0800

    testing setup

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,14 @@
 
 int main(int argc, char *argv[]) {
   const char *config_path = std::getenv("HEULPAD_CONFIG");
+  const char *plugins = std::getenv("HEULPAD_PLUGINS");
+  const char *registry = std::getenv("HEULPAD_PLUGINS_REGISTRY");
   if (!config_path) {
     printf("Could not find config path.\n");
   } else {
     printf("[SUCCESS] %s\n", config_path);
+    printf("[SUCCESS] %s\n", plugins);
+    printf("[SUCCESS] %s\n", registry);
   }
   return 0;
 }
