e4a1fa3d5eea8a25af8dbac2d8d807356d06e095
grabbiel <fcruzado22@gmail.com>
2024-12-04 20:28:58 -0800

    testing out separate binaries?

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,18 @@
 #include <cstdio>
 #include <cstdlib>
-
+/*
+ * constants:
+ * HEULPAD_PLUGINS
+ * HEULPAD_PLUGINS_REGISTRY
+ * HEULPAD_CONFIG
+ *
+ * */
 int main(int argc, char *argv[]) {
-  const char *config_path = std::getenv("HEULPAD_CONFIG");
-  const char *plugins = std::getenv("HEULPAD_PLUGINS");
-  const char *registry = std::getenv("HEULPAD_PLUGINS_REGISTRY");
-  if (!config_path) {
-    printf("Could not find config path.\n");
-  } else {
-    printf("[SUCCESS] %s\n", config_path);
-    printf("[SUCCESS] %s\n", plugins);
-    printf("[SUCCESS] %s\n", registry);
+
+  printf("Argument count is %d\n", argc);
+  for (int i = 0; i < argc; ++i) {
+    printf("ar%d: %s\n", i + 1, argv[i]);
   }
+
   return 0;
 }
