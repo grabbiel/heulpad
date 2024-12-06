ff8c3695059d4ce04f5274cad25882fb97b8d87d
grabbiel <fcruzado22@gmail.com>
2024-12-05 17:23:42 -0800

    test help

---
+++ src/commands/help/main.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+#include <string.h>
+
+#define NCOMMANDS 5
+#define NMAINFLAGS 5
+const char *commands[NCOMMANDS] = {"new", "enable", "help", "preview", "list"};
+const char *flags[NMAINFLAGS] = {"--version", "--help", "--config", "--logs",
+                                 "--env"};
+
+int main(int argc, char *argv[]) {
+  if (argc > 1) {
+    for (int i = 0; i < NCOMMANDS; ++i) {
+      if (strcmp(argv[1], commands[i]) == 0) {
+        printf("[help]: Invoking man page 'heulpad-%s'\n", commands[i]);
+        return 0;
+      }
+    }
+    fprintf(stderr, "[help]: Invalid <command>\n");
+    return 1;
+  }
+
+  const char *prefix = "usage: heulpad ";
+  const int prefix_len = strlen(prefix);
+  printf("%s[%s] [%s]\n", prefix, flags[0], flags[1]);
+  printf("%*s[%s] [%s]\n", prefix_len, "", flags[2], flags[3]);
+  printf("%*s[%s]\n\n\n", prefix_len, "", flags[4]);
+
+  return 0;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 /*
  * constants:
  * HEULPAD_PLUGINS
@@ -7,11 +8,28 @@
  * HEULPAD_CONFIG
  *
  * */
+
+// All available commands
+#define NCOMMANDS 5
+const char *commands[NCOMMANDS] = {"new", "enable", "help", "preview", "list"};
+
 int main(int argc, char *argv[]) {
   if (argc > 1) {
-    printf("Will call 'heulpad %s'\n", argv[1]);
-    printf("Looking through %s\n", std::getenv("HEULPAD_LIBEXEC"));
-  } else
-    printf("Calling 'heulpad'\n");
+    for (int i = 0; i < NCOMMANDS; ++i) {
+      if (strcmp(argv[1], commands[i]) == 0) {
+        printf("Will call 'heulpad %s'\n", argv[1]);
+        printf("Looking through %s/heulpad-%s\n",
+               std::getenv("HEULPAD_LIBEXEC"), argv[1]);
+        return 0;
+      }
+    }
+    fprintf(
+        stderr,
+        "'heulpad %s' is not a command\t\tSee heulpad --help for reference\n",
+        argv[1]);
+    return 1;
+  }
+  printf("Calling 'heulpad help'\n");
+  printf("Looking through %s/heulpad-help\n", std::getenv("HEULPAD_LIBEXEC"));
   return 0;
 }
