90e9747c3c9ab40456706ab49b07b54e46b615f9
grabbiel <fcruzado22@gmail.com>
2024-12-10 20:52:02 -0800

    tesssst

--- src/commands/help/main.cpp
+++ src/commands/help/main.cpp
@@ -11,6 +11,7 @@ const char *flags[NMAINFLAGS] = {"--version", "--help", "--config", "--logs",
 int main(int argc, char *argv[]) {
   if (argc > 0) {
     for (int i = 0; i < NCOMMANDS; ++i) {
+      printf("argv[1] = %s\n", argv[1]);
       if (strcmp(argv[1], commands[i]) == 0) {
         /* ============================
          * System call to man-page can be at most:
