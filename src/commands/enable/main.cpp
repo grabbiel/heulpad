1fb37a487a8a0eaf559489253ac8693eb3b200e8
grabbiel <fcruzado22@gmail.com>
2024-12-10 20:47:17 -0800

    fixing argc woopsie

--- src/commands/enable/main.cpp
+++ src/commands/enable/main.cpp
@@ -1,7 +1,7 @@
 #include <cstdio>
 int main(int argc, char *argv[]) {
-  if (argc > 1) {
-    for (int i = 1; i < argc; ++i) {
+  if (argc > 0) {
+    for (int i = 0; i < argc; ++i) {
       printf("heulpad enable %s\n", argv[i]);
     }
     return 1;
