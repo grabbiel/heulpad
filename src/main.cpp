86d2409b7bf005313ac2320f743cd78a8b3e811f
grabbiel <fcruzado22@gmail.com>
2024-12-10 22:22:14 -0800

    fkdfd

--- src/main.cpp
+++ src/main.cpp
@@ -17,16 +17,19 @@ extern char **environ;
 const char *commands[NCOMMANDS] = {"new", "enable", "help", "preview", "list"};
 
 int main(int argc, char *argv[]) {
+  const char *libexecpath = std::getenv("HEULPAD_LIBEXEC");
+  if (libexecpath == nullptr) {
+    fprintf(stderr, "Cannot determine environment variable HEULPAD_LIBEXEC");
+    return 1;
+  }
+  char manpath[256];
+  snprintf(manpath, sizeof(manpath), "MANPATH=%s/man",
+           std::getenv("HEULPAD_SHARE"));
+  const char *minimal_env[] = {"TERM=xterm-256color", manpath, NULL};
+  char exec_path[165] = "";
   if (argc > 1) {
     for (int i = 0; i < NCOMMANDS; ++i) {
       if (strcmp(argv[1], commands[i]) == 0) {
-        printf("Will call 'heulpad %s'\n", argv[1]);
-        const char *libexecpath = std::getenv("HEULPAD_LIBEXEC");
-        if (libexecpath == nullptr) {
-          fprintf(stderr,
-                  "Cannot determine environment variable HEULPAD_LIBEXEC");
-          return 1;
-        }
         /* ============================
          * The path to executable would be:
          * ============================
@@ -40,13 +43,8 @@ int main(int argc, char *argv[]) {
          *                          163
          *                         ~165
          * */
-        char exec_path[165] = "";
         snprintf(exec_path, sizeof(exec_path), "%s/heulpad-%s", libexecpath,
                  argv[1]);
-        char manpath[256];
-        snprintf(manpath, sizeof(manpath), "MANPATH=%s/man",
-                 std::getenv("HEULPAD_SHARE"));
-        const char *minimal_env[] = {"TERM=xterm-256color", manpath, "\0"};
         return execve(exec_path, argv + 2, (char *const *)minimal_env);
       }
     }
@@ -57,7 +55,6 @@ int main(int argc, char *argv[]) {
         argv[1]);
     return 1;
   }
-  printf("Calling 'heulpad help'\n");
-  printf("Looking through %s/heulpad-help\n", std::getenv("HEULPAD_LIBEXEC"));
-  return 0;
+  snprintf(exec_path, sizeof(exec_path), "%s/heulpad-help", libexecpath);
+  return execve(exec_path, NULL, (char *const *)minimal_env);
 }
