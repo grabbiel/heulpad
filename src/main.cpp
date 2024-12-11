bff41d2b21ca67797732ca74b85b9cb1c5061247
grabbiel <fcruzado22@gmail.com>
2024-12-11 14:58:09 -0800

    testing popen

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,12 @@
 #include <unistd.h>
 /*
  * constants:
- * HEULPAD_PLUGINS
- * HEULPAD_PLUGINS_REGISTRY
- * HEULPAD_CONFIG
- *
+ * HEULPAD_MAN
+ * HEULPAD_CONFIG: "#{etc}/heulpad/config",
+ * HEULPAD_PLUGINS: "#{var}/heulpad/plugins",
+ * HEULPAD_PLUGINS_REGISTRY: "#{share}/heulpad/plugins.list",
+ * HEULPAD_LIBEXEC: libexec,
+ * HEULPAD_SHARE: "#{share}"
  * */
 
 // All available commands
@@ -20,10 +22,7 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "Cannot determine environment variable HEULPAD_LIBEXEC");
     return 1;
   }
-  char manpath[256];
-  snprintf(manpath, sizeof(manpath), "MANPATH=%s/man",
-           std::getenv("HEULPAD_SHARE"));
-  const char *minimal_env[] = {"TERM=xterm-256color", manpath, NULL};
+  const char *minimal_env[] = {"TERM=xterm-256color", NULL, NULL};
   char exec_path[165] = "";
   if (argc > 1) {
     for (int i = 0; i < NCOMMANDS; ++i) {
@@ -41,6 +40,26 @@ int main(int argc, char *argv[]) {
          *                          163
          *                         ~165
          * */
+        if (strcmp(argv[1], "help") == 0) {
+          FILE *fp;
+          char manpath[1024];
+          fp = popen("manpath", "r");
+          if (!fp) {
+            fprintf(stderr, "[heulpad]: Could not execute 'manpath' command\n");
+            return 1;
+          }
+          while (fgets(manpath, 1024, fp) != NULL) {
+            printf("%s\n", manpath);
+          }
+          if (pclose(fp) == -1) {
+            fprintf(stderr, "[heulpad]: Could not close buffer\n");
+            return 1;
+          }
+          // char manpath[256];
+          // snprintf(manpath, sizeof(manpath), "MANPATH=%s/man",
+          //          std::getenv("HEULPAD_SHARE"));
+          return 0;
+        }
         snprintf(exec_path, sizeof(exec_path), "%s/heulpad-%s", libexecpath,
                  argv[1]);
         return execve(exec_path, argv + 2, (char *const *)minimal_env);
